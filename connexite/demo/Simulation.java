4dcf6224acb4135abfd7b31f53784da688249975
Merwan Achibet <merwandonut@hotmail.com>
2012-02-18 00:14:09 +0100

    connexite: code neighbors checking

--- connexite/demo/Simulation.java
+++ connexite/demo/Simulation.java
@@ -10,15 +10,21 @@ public class Simulation {
 
 	private int worldSize;
 	private int sensorCount;
+	private int sensorRadius;
 
 	private int frameLength = 1000 / 60;
+
+	private static String style =
+		"edge { size: 1px; }";
 
 	public Simulation() {
 
 		this.net = new SingleGraph("sensor network");
+		this.net.addAttribute("ui.stylesheet", Simulation.style);
 
 		this.worldSize = 1000;
-		this.sensorCount = 50;
+		this.sensorCount = 500;
+		this.sensorRadius = 100;
 
 		for(int i = 0; i < this.sensorCount; ++i)
 			this.spawn();
@@ -41,7 +47,7 @@ public class Simulation {
 		Double x_old = sensor.getAttribute("x");
 		Double y_old = sensor.getAttribute("y");
 
-		// Compute the ew position.
+		// Compute the new position.
 		double x_new = x_old == null ? x : x + x_old;
 		double y_new = y_old == null ? y : y + y_old;
 
@@ -50,11 +56,23 @@ public class Simulation {
 		sensor.setAttribute("y", y_new + y);
 	}
 
+	private double distance(Node s1, Node s2) {
+
+		// Retrieve the two positions.
+		Double x1 = s1.getAttribute("x");
+		Double y1 = s1.getAttribute("y");
+		Double x2 = s2.getAttribute("x");
+		Double y2 = s2.getAttribute("y");
+
+		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
+
 	public void run() {
 
 		while(true) {
 
 			this.update();
+
 			this.pause(this.frameLength);
 		}
 	}
@@ -74,11 +92,36 @@ public class Simulation {
 		for(Node sensor : this.net)
 			this.updatePosition(sensor);
 
+		this.checkForNeighbors();
 	}
 
 	private void updatePosition(Node sensor) {
 
 		this.move(sensor, Math.random(), Math.random());
+	}
+
+	private void checkForNeighbors() {
+
+		Object[] sensors = this.net.getNodeSet().toArray();
+
+		for(int i = 0, l = sensors.length; i < l; ++i)
+			for(int j = i + 1; j < l; ++j) {
+
+				Node s1 = (Node)sensors[i];
+				Node s2 = (Node)sensors[j];
+
+				double d = this.distance(s1, s2);
+				//System.out.println(s1.getId()+" "+s2.getId()+" "+d+" "+s1.hasEdgeBetween(s2));
+				// If the sensors are already linked.
+				if(s1.hasEdgeBetween(s2)) {
+
+					if(d > this.sensorRadius)
+						this.net.removeEdge(s1, s2);
+				}
+				// If the sensors are not linked yet.
+				else if(d <= this.sensorRadius)
+					this.net.addEdge(s1.getId() + "_" + s2.getId(), s1, s2);
+			}
 	}
 
 }
